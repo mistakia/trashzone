b24467531efdbc51c3b20c521fe5dadbaaf24dee
Kia Rahimian <kia@tintmail.com>
2019-09-05 17:44:32 -0400

    chore: update to 2019

--- config.js
+++ config.js
@@ -1,12 +1,12 @@
 const moment = require('moment')
 
 module.exports = {
-  week_one: moment('2018-08-28'),
+  week_one: moment('2019-09-03'),
   pff: {
     leagueId: 'preset_std'
   },
   espn: {
     leagueId: 147002,
-    seasonId: 2018
+    seasonId: 2019
   }
 }
